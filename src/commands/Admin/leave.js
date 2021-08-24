014cad1f29f099a9f2e5ccb78d883b48470b0073
Michuuu_ <56083047+michuuu-x@users.noreply.github.com>
2021-08-24 19:26:01 +0200

    Update leave.js

--- src/commands/Admin/leave.js
+++ src/commands/Admin/leave.js
@@ -10,7 +10,7 @@ module.exports = {
       */
 
       run: async(client, message, args) => {
-          if(!message.author.id === client.ownerId) return message.reply('Its an owner only command (Only my owner can use me!)');
+          if(!message.author.id === client.ownerId) return message.reply('Jest to polecenie tylko dla właściciela (Tylko mój właściciel może mnie używać!)');
           client.emit('guildMemberRemove', message.member) // you can also use this for any necesary events!
       }
 }
